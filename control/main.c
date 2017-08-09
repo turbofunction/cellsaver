d6f2bfc992fa891365b0e21e186b0b647d17a302
Janne Savukoski <janne@savukoski.name>
2017-08-09 20:53:18 +0300

    Firmware projects init

---
+++ control/main.c
@@ -0,0 +1,10 @@
+// created 09/08/2017 20.35.21
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while (1) 
+    {
+    }
+}
---
+++ power/main.c
@@ -0,0 +1,10 @@
+// created 09/08/2017 20.43.02
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while (1) 
+    {
+    }
+}
